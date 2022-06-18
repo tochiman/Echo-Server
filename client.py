418cf204b6e9e10dbf24241d26334ad63e3fa043
tochiman <yuutotochizawa@gmail.com>
2022-06-18 21:57:30 +0900

    class

--- client.py
+++ client.py
@@ -1,18 +1,33 @@
 import socket
 import sys
 
-host = 'localhost'
-port = 8080
-server_address = (host, port)
+class Main():
+    def __init__(self) -> None: 
+        self.host = 'localhost'
+        self.port = 8080
+        self.server_address = (self.host, self.port)
+        self.recv_size = 1024
 
-recv_size = 1024
+    def content_send(self,message):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.connect(self.server_address)
+            client_socket.send(message.encode('utf-8'))
 
-message = sys.argv[1]
+            data = client_socket.recv(self.recv_size)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-    client_socket.connect(server_address)
-    client_socket.send(message.encode('utf-8'))
+            print(data.decode('utf-8'))
 
-    data = client_socket.recv(recv_size)
+if '__main__' == __name__:
+    main = Main()
+    message = sys.argv[1]
 
-    print(data.decode('utf-8'))
+    if message == 'loop':
+        while True:
+            message = input("Please enter the words you want to send")
+            if message == 'break':
+                break
+            else:
+                main.content_send(message=message)
+    else:
+        main.content_send(message=message)
+
